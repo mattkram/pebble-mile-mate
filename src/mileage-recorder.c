3022b9f58145edc5b134fdf2d126247dc9662ef2
Matt Kramer <matthew.robert.kramer@gmail.com>
2016-02-25 20:13:26 -0600

    Added all

---
+++ src/mileage-recorder.c
@@ -0,0 +1,23 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static MenuLayer *s_menu_layer;
+
+static void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
