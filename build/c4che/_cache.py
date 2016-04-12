b06588e45e5a7b8fa48a75a2df0ca9c0aa943919
Matt Kramer <matthew.robert.kramer@gmail.com>
2016-04-12 18:07:33 -0500

    Remove build directory

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/mattkram/Library/Application Support/Pebble SDK/SDKs/current/sdk-core/pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = ['chalk']
